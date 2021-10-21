d50444ee19b28c924e73ba02736b3a7d6d77aa21
Matt <Mattxiong_14@hotmail.com>
2021-10-21 09:40:07 -0500

    added login and register to client side.

--- KicksKollector/client/kickskollector/src/App.js
+++ KicksKollector/client/kickskollector/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Spinner } from "reactstrap";
+import ApplicationViews from "./components/ApplicationViews";
+import { onLoginStatusChange } from "./modules/authManager";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+
+  useEffect(() => {
+    onLoginStatusChange(setIsLoggedIn);
+  }, []);
+
+  if (isLoggedIn === null) {
+    return <Spinner className="app-spinner dark" />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ApplicationViews isLoggedIn={isLoggedIn} />
+    </Router>
   );
 }
 
